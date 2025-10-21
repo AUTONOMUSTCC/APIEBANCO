b805270bbdae8c589a2f197e27ff5f95bf37ac91
natemy <emynathalia03@gmail.com>
2025-10-20 21:58:53 -0300

    Adicionar arquivos de projeto.

---
+++ api/Autonomus/Business/PublicacaoPrestadorAbordagemBO.cs
@@ -0,0 +1,49 @@
+﻿using Autonomus.ContextNameSpace;
+using Autonomus.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autonomus.Business
+{
+    public class PublicacaoPrestadorAbordagemBO
+    {
+
+        public List<PublicacaoPrestadorAbordagem> ObterAbordagemPrestador()
+        {
+            using var contexto = new Context();
+            var resultado = contexto.Set<PublicacaoPrestadorAbordagem>()
+                .FromSqlRaw("EXEC sp_ObterAbordagensPrestador");
+
+            return resultado.ToList();
+        }
+        public decimal InserirAbordagemPrestador(PublicacaoPrestadorAbordagem abordagem)
+        {
+            using Context contexto = new Context();
+
+            var parametros = new[]
+            {
+                new SqlParameter("@IdPublicacaoPrestador", abordagem.IdPublicacaoPrestador),
+                new SqlParameter("@DescricaoAbordagemPrestador", abordagem.DescricaoAbordagemPrestador ?? (object)DBNull.Value)
+            };
+
+            var resultado = contexto
+                .Set<NovoAbordagemPrestadorResultado>()
+                .FromSqlRaw("EXEC sp_InserirAbordagemPrestador @IdPublicacaoPrestador, @DescricaoAbordagemPrestador", parametros)
+                .AsEnumerable()
+                .FirstOrDefault();
+
+            return resultado?.NovoIdPrestadorAbordagem ?? 0;
+        }
+
+        public void DeletarAbordagemPrestador(int IdAbordagemPrestador)
+        {
+            var contexto = new Context();
+            var parametros = new[]
+            {
+                new SqlParameter("@IdAbordagem", IdAbordagemPrestador)
+            };
+
+            contexto.Database.ExecuteSqlRaw("EXEC sp_DeletarAbordagemPrestador @IdAbordagem ", parametros);
+        }
+    }
+}
---
+++ api/Autonomus/Business/PublicacaoPrestadorQualidadesBO.cs
@@ -0,0 +1,51 @@
+﻿using Autonomus.ContextNameSpace;
+using Autonomus.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autonomus.Business
+{
+    public class PublicacaoPrestadorQualidadesBO
+    {
+
+        public List<PublicacaoPrestadorQualidades> ObterQualidadesPrestador()
+        {
+            using var contexto = new Context();
+            var resultado = contexto.Set<PublicacaoPrestadorQualidades>()
+                .FromSqlRaw("EXEC sp_ObterQualidadesPrestador");
+
+            return resultado.ToList();
+        }
+
+
+        public decimal InserirQualidadesPrestador(PublicacaoPrestadorQualidades qualidade)
+        {
+            using Context contexto = new Context();
+
+            var parametros = new[]
+            {
+                new SqlParameter("@IdPublicacaoPrestador", qualidade.IdPublicacaoPrestador),
+                new SqlParameter("@DescricaoQualidadePrestador", qualidade.DescricaoQualidadePrestador ?? (object)DBNull.Value)
+            };
+
+            var resultado = contexto
+                .Set<NovoQualidadePrestadorResultado>()
+                .FromSqlRaw("EXEC sp_InserirQualidadePrestador @IdPublicacaoPrestador, @DescricaoQualidadePrestador", parametros)
+                .AsEnumerable()
+                .FirstOrDefault();
+
+            return resultado?.NovoIdPrestadorQualidade ?? 0;
+        }
+
+        public void DeletarQualidadesPrestador(int IdQualidadePrestador)
+        {
+            var contexto = new Context();
+            var parametros = new[]
+            {
+                new SqlParameter("@IdQualidade", IdQualidadePrestador)
+            };
+
+            contexto.Database.ExecuteSqlRaw("EXEC sp_DeletarQualidadePrestador @IdQualidade ", parametros);
+        }
+    }
+}
